d6f17758fa9e03b59cfb1403071db895890bb275
Marek Goldmann <marek.goldmann@gmail.com>
2013-01-07 13:10:10 +0100

    Enable debug logging for DataMapper (will show SQL)

--- torquebox_init.rb
+++ torquebox_init.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'models/term'
 require 'models/url'
 
-DataMapper::Logger.new(TorqueBox::Logger.new(DataMapper), :info)
+DataMapper::Logger.new(TorqueBox::Logger.new(DataMapper), :debug)
 DataMapper.setup(:default, ENV['DB_URL'])
 DataMapper.auto_upgrade!
 
